097b1d0f5d3105725b861d70b5f6c9568c41f4dc
Parvsub <Praveenboda421@gmail.com>
2023-02-14 10:59:51 +0530

    Added: Display the current board

--- tictactoe/TicTacToeGame.java
+++ tictactoe/TicTacToeGame.java
@@ -9,7 +9,7 @@ public class TicTacToeGame {
     Scanner scanner = new Scanner(System.in);
 
     public void initialize() {
-        for (i = 1; i < 10; i++) {   //initialize
+        for (i = 1; i < 10; i++) {
             board[i] = ' ';
         }
     }
@@ -20,20 +20,26 @@ public class TicTacToeGame {
         if (choice == 'X') {
             player = 'X';
             computer = 'O';
-        } else if (choice == 'Y') {
+        } else if (choice == 'O') {
             player = 'O';
             computer = 'X';
         }
     }
 
-    public static void main(String[] args) {
+    public void showboard() {
+
         System.out.println("Welcome to Tic Tac Toe");
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-        TicTacToeGame obj1 = new TicTacToeGame();
-        obj1.check();
+    }
+
+    public static void main(String[] args) {
+
+        TicTacToeGame TicTacObject = new TicTacToeGame();
+        TicTacObject.initialize();
+        TicTacObject.showboard();
     }
 }
